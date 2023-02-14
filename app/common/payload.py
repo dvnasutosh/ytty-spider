4a1c32dfe2a067d2cf15bc0ee40a173bd61897bb
Dvain84143 <dvnasutosh@gmail.com>
2023-02-14 20:14:13 +0530

    added common payload data

---
+++ app/common/payload.py
@@ -0,0 +1,11 @@
+
+
+generalPayload=lambda videoId: {
+    "videoId": videoId,
+    "context": {
+        "client": {
+        "clientName": "WEB_EMBEDDED_PLAYER",
+        "clientVersion": "1.20230131.01.00",
+        },
+    }
+    }
