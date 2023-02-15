9bcccc029170be4420ee43d9ec442878bad676f5
Dvain84143 <dvnasutosh@gmail.com>
2023-02-15 21:29:25 +0530

    Updated URL

--- app/common/urls.py
+++ app/common/urls.py
@@ -1,5 +1,8 @@
 
+# from dataclasses import dataclass
+
+
 url={
-    'player':r'https://www.youtube.com/youtubei/v1/player?key=AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8&prettyPrint=false',
-    'next':r'https://www.youtube.com/youtubei/v1/next?key=AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8&prettyPrint=false'
+    'player':r'https://www.youtube.com/youtubei/v1/player?key=AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8e',
+    'next':r'https://www.youtube.com/youtubei/v1/next?key=AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8',
 }
