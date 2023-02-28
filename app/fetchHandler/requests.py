4b5caf800b7d2c4792d2102a7397323435a3ba62
Dvain84143 <dvnasutosh@gmail.com>
2023-02-28 15:42:16 +0530

    Formulated and implemented a common modularised re usable(With minute changes ofc) yt_requests class and base_endpoint class

--- app/fetchHandler/requests.py
+++ app/fetchHandler/requests.py
@@ -1,8 +1,8 @@
 from urllib.parse import urlencode
 import requests
 from typing import Any
-
-    
+import json
+from enum import Enum
 class ResourceName(Enum):
     API:str=r'https://www.youtube.com/'
     # SHint:str=r'https://suggestqueries-clients6.youtube.com/'
@@ -21,7 +21,11 @@ class queryString:
     
 class URL:
     def __init__(self,Endpoint:str) -> None:
-         self.__data=ResourceName.API+Route(Endpoint)+"?"+urlencode(queryString.basic)
+         self.__data=ResourceName.API
+         +Route(Endpoint)
+         +"?"
+         +urlencode(queryString.basic)
+         
     def __repr__(self) -> str:
         return self.__data
     
@@ -30,10 +34,57 @@ class Header:
         if not Auth:
             self.header=None
 
+class PF:
+    EMBED={
+    "client": {
+        "clientName": "WEB_EMBEDDED_PLAYER",
+        "clientVersion": "1.20230131.01.00",
+    }}
+    WEB={
+        "client": {
+            "clientName": "WEB",
+            "clientVersion": "2.20230213.01.00"
+        }
+    }
 
-class Requests:
-    def __init__(self,API:object):
-        self.URL=URL(API.__name__)
-        self.payload=Payload(API())
-        self.Header=Header()
+
+class Payload:
+    def __init__(self,Uparam:dict,PF:PF=PF.WEB,geoLocation='in') -> None:
+        self._data={'context':PF}
+        self._data['context']['gl']=geoLocation
+        for i, j in Uparam:
+            self._data[i]=j
+    def __repr__(self) -> str:
+        return json.dumps(self._data)
+    def __call__(self) -> dict:
+        self._data
+    
+
+class yt_requests:
+    def __init__(self):
+        self.URL:URL
+        self.PAYLOAD:Payload
+        self.Header:Header
+
+    def __init_subclass__(cls) -> None:
+        if not issubclass(cls,Endpoint_Base):
+            raise TypeError('doesn\'t inherit Endpoint_Base')
         
+        cls.URL=URL(cls.__name__)
+        cls.PAYLOAD=Payload(Uparam=cls(),PF=cls.PF)
+        cls.Header=Header()
+    def Fetch(self):
+        return requests.Request(url=self.URL,data=self.PAYLOAD,headers=self.Header)
+    
+
+class Endpoint_Base:
+    def __init__(self) -> None:
+        self.PF:PF=PF.WEB
+        self.gl:PF='In'
+    def __str__(self) -> str:
+        return self.__name__
+    def __repr__(self)->str:
+        return json.dumps(self.__dict__)
+    def __call__(self)->dict:
+        return self.__dict__
+      
