76c036d93361b4e331712575f767c00b1b2687bb
Dvain84143 <dvnasutosh@gmail.com>
2023-03-01 19:53:33 +0530

    Updated StrictDictionary to be able to set default value based on the values set in class member data  defination of inherited child classes

--- app/type/Base.py
+++ app/type/Base.py
@@ -46,27 +46,49 @@ class StrictDictionary(Dictionary):
     """
     def __init__(self, **kwargs) -> None:
         """
-        Initializes the object with the given keyword arguments.
-        If no arguments are given, the attributes are set to the default values obtained by calling the corresponding attribute functions.
+        Initialize any given data to be stored. The default datas are stored regardless 
         """
-        if not kwargs:
-            # Iterate over the class annotations and set the attributes
-            # based on the default values obtained by calling the corresponding attribute functions
-            for i, j in self.__annotations__.items():
-                # Check if the attribute type is a function that can be called
-
+        # Adding default values
+        for i,j in self.idata.items():
+            super().__setitem__(i,j)
+        if kwargs:
+            # if kwargs exists adding all the given values using super class
+            for i,j in kwargs.items():
+                
+                #SECTION: validation logic
+                expected_type=self.__annotations__[i] if not get_origin(self.__annotations__[i]) else get_origin(self.__annotations__[i])
+                if not type(j) == expected_type:
+                    raise TypeError(
+                        f'{i} key has a value {j} which is of type {type(j)}. {expected_type} expected.')
+                #!SECTION
+                
+                super().__setitem__(i,j)
+                
+    def __init_subclass__(cls) -> None:
+        cls.idata=dict()
+        for i, j in cls.__annotations__.items():
+            if i in cls.__dict__.keys():
+                
+                #validation logic
+                # Checking if expected_type is of Typing Class than converting it into it's base class
+                expected_type=j if not get_origin(j) else get_origin(j)
+                
+                # Raise error if the given value is not of the expected type
+                if not type(cls.__dict__[i]) == expected_type:
+                    raise TypeError(
+                        f'{i} key has a value {cls.__dict__[i]} which is of type {type(cls.__dict__[i])}. {expected_type} expected.')
+                
+                cls.idata[i]=cls.__dict__[i]
+            else:
                 if type(j) is type:
-                    super().__setitem__(i, j())
-                elif get_origin(j):
-                    super().__setitem__(i, j.__origin__())
+                    cls.idata[i]=j()
                     
+                elif get_origin(j):
+                    cls.idata[i]=j.__origin__()
                 else:
                     raise ValueError(f"can't assign {type(j)} data. Illegal Value.")
-        else:
-            # Call the superclass constructor to set the attributes based on the given keyword arguments
-            super().__init__(**kwargs)
-    def validate(self,__name,__value):
-        pass
+
+        
     def __setattr__(self, __name: str, __value: Any) -> None:
         """
         Overrides the superclass `__setattr__` method to perform type checking before assignment.
@@ -87,6 +109,22 @@ class StrictDictionary(Dictionary):
 
 
     def __setitem__(self, __name, __value) -> None:
+        """
+        Overrides the superclass `__setitem__` method to perform type checking before assignment.
+        If the value to be assigned is not of the expected type, a `TypeError` is raised.
+        """
+        expected_type=self.__annotations__[__name]
+
+        # verifying for Typing types
+        if get_origin(expected_type):
+            expected_type=expected_type.__origin__
+
+        if not type(__value) == expected_type:
+            raise TypeError(
+                f'{__name} key has a value {__value} which is of type {type(__value)}. {self.__annotations__[__name]} expected.')
+        else:
+            super().__setitem__(__name, __value)
+
         """
         Overrides the superclass `__setitem__` method to perform type checking before assignment.
         If the value to be assigned is not of the expected type, a `TypeError` is raised.
