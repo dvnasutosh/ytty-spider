f141fc0e90399f62c323c18660cf3360be019726
Dvain84143 <dvnasutosh@gmail.com>
2023-03-07 02:09:18 +0530

    Finally the phase 1 of the archietecture is complete Endpoints next and player working perfectly

--- app/fetchHandler/Endpoint.py
+++ app/fetchHandler/Endpoint.py
@@ -2,18 +2,29 @@
 from app.fetchHandler.Requests import endpoint_base,yt_requests,CONTEXT
 from app.authentication.auth import Authentication
 
+import json
 
 class next(yt_requests,endpoint_base):
     
-        
-    def __call__(self,videoId:str='',continuation:str='')->None:
+    def __call__(self,videoId:str='',continuation:str=''):
         if videoId=='' and continuation=='':
             raise ValueError('Either video_id or continuation must be provided.')
+
         self.PAYLOAD.setUParam(videoId=videoId,continuation=continuation)
+
         self.raw=self.Fetch()
         return self.raw
                 
 
+class player(yt_requests,endpoint_base):
+    
+    def __call__(self,videoId:str=''):
+
+        self.PAYLOAD.setUParam(videoId=videoId)
+        
+
+        self.raw=self.Fetch()
+        return self.raw
 
 
 
