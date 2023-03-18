3bb421cc4ff4d9e6d399ac560ab1d8ff4b9ec053
Asutosh_Rath <dvnasutosh@gmail.com>
2023-03-18 17:10:03 +0530

    cleared the file of unnecesasary cimments and print statements

--- app/type/Video.py
+++ app/type/Video.py
@@ -130,22 +130,3 @@ class Downloadables(StrictDictionary):
     muxed:streamingList
     unmuxed:adaptiveList
 
-
-
-# video = Video()
-# video['videoId'] = '123456'
-# video['title'] = 'My Video Title'
-# video['author'] = 'Author Name'
-# video['lengthSeconds'] = '600'
-# video['keywords'] = ['video', 'content', 'entertainment']
-# video['channelId'] = 'ABC123'
-# video['shortDescription'] = 'A short description of the video'
-# video['video'] = [{'url': 'http://video.com/video1', 'format': 'mp4'}, {'url': 'http://video.com/video2', 'format': 'webm'}]
-# video['isOnwerViewing'] = True
-# video['isCrawlable'] = False
-# video['allowRating'] = True
-# video['isPrivate'] = False
-# video['isLiveContent'] = False
-# print(video)
-
-print(isinstance(adaptiveVideo, adaptiveAudio))
