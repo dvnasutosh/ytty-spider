d84c40ed443e78ab58c3beac6cd9101af7ffa5e8
Asutosh_Rath <dvnasutosh@gmail.com>
2023-03-23 18:34:05 +0530

    debugged interaction data fetcher
    corrected probable error in __init__ of pre fetching data if videoId is mentioned. set self.videoId  instead

--- app/services/videoManager.py
+++ app/services/videoManager.py
@@ -17,26 +17,42 @@ class videoManager:
 
         self.next = next(auth=auth, client=context)
         self.player = player(auth=auth, client=context)
+
         if videoId:
-            self.details = self.fetchDetails(videoId)
+            self.videoId = videoId
     
     def updateContext(self, context: CONTEXT):
         self.context=context
         self.next.UpdateContext(context)
         self.player.UpdateContext(context)
 
-    def fetchDetails(self, videoId: str):
+    def videoExists(self,videoId:str):
+        if not videoId:
+            if not self.videoId:
+                raise AttributeError('videoId Not set')
+            return self.videoId
+        else:
+            return videoId
+
+    def Details(self, videoId: str=str()):
+        
+        videoId=self.videoExists(videoId)
+        
         return Deserialise.videoData(loads(self.player(videoId).text))
-        # return loads(self.player(videoId).text)
     
+    def interactionData(self,videoId:str =str()):
+        videoId=self.videoExists(videoId)
+        
+        interactionData=Deserialise.interactionData( loads(self.next(videoId).text))
+        return interactionData
+
     def setvideoId(self,videoId:str):
         self.videoId=videoId
 
     def Download(self,videoId:str=str()):
-        if not videoId:
-            if not self.videoId:
-                raise AttributeError('videoId Not set')
-            videoId=self.videoId
+
+        videoId=self.videoExists(videoId)
+
         downloadRequest=player(self.auth,self.context)
         
         #   Initial Requests
@@ -56,10 +72,13 @@ class videoManager:
             return 'cipheredData:Deciphering To be Implemented'
         
         # Extracting Data
-
         return Deserialise.streamingData(raw) 
 
+    def comments(self,videoId:str=str()):
+        return "to be implemented"
+        
 """
+
 Sample videoIds
 rXMX4YJ7Lks -> Age restricted Data
 """
