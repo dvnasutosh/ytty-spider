9851d30a5aef0aeed32010d3d5137a4214db73af
Asutosh Rath <dvnasutosh@gmail.com>
2023-04-06 21:36:02 +0530

    added playlist dataclasses

---
+++ app/dataclass/playlistDataClasses/Playlist.py
@@ -0,0 +1,23 @@
+from app.dataclass.Base import StrictDictionary
+from app.dataclass.common import img, strbool
+from app.dataclass.ChannelDataClasses.ChannelMeta import browseEndpoint
+
+class Playlist(StrictDictionary):
+    title:str
+    playlistId:str
+    description:str
+    privacy:str
+    canDelete:strbool
+    isEditable:strbool
+    canShare:strbool
+    videosCount:int
+    ownerEndpoint:browseEndpoint
+    playlistHeaderBanner:img
+    playlistViews:int
+
+class PlaylistMini(StrictDictionary):
+    ownerBadges:bool
+    playlistId:bool
+    thumbnail:img
+    title:str
+    videosCount:int
