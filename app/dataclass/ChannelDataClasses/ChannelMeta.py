e6c4229cf753c556366c411a8b58c11d73cabaa9
Asutosh Rath <dvnasutosh@gmail.com>
2023-04-06 21:31:40 +0530

    Added channel specific endpoints

--- app/dataclass/ChannelDataClasses/ChannelMeta.py
+++ app/dataclass/ChannelDataClasses/ChannelMeta.py
@@ -1,16 +1,27 @@
+from typing import List
 from app.dataclass.Base import StrictDictionary
 from app.dataclass.common import img,strbool,strList
+from app.FetchEngine.endpoint import browse
 
+class browseEndpoint(StrictDictionary):
+    browseId:str
+    params:str
+    canonicalBaseUrl:str
+    
 class tabEndpoints(StrictDictionary):
     title:str
     selected:strbool
-    params:str
+    browseEndpoint:browseEndpoint
 
 class tabEndpointsList(list):
-    def __init__(self,args:list=list()):
+    def __init__(self,args:List[tabEndpoints]=list()):
         if not all([isinstance(i, tabEndpoints) for i in args]):
             raise ValueError('tabEndpointsList will need to have "tabEndpoints" type items only')
         super().__init__(args)
+    
+class tabData(StrictDictionary):
+    tabs:tabEndpointsList
+    content:list
 
 class Channel(StrictDictionary):
     channelId:str
@@ -29,4 +40,12 @@ class Channel(StrictDictionary):
     isunlisted:strbool
     tags:strList
     tabs:tabEndpointsList
+
+class channelMini(StrictDictionary):
+    channelId:str
+    title:str
+    thumbnail:img
+    videoCount:int
+    subscriberCount:int
+    isVerified:bool    
     
