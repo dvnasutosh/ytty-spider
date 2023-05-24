a53b8a4cb253642b47111c535958767b719fdba4
Asutosh Rath <dvnasutosh@gmail.com>
2023-05-24 11:21:48 +0530

    Added new StrictDictionary setup. Fixed few refactoring problems

---
+++ app/dataclass/videoDC/Comments.py
@@ -0,0 +1,32 @@
+
+from app.dataclass.common import img, publishTime
+from betterdataclass.StrictDictionary import StrictDictionary
+from typing import Union,List
+
+
+class authorSimple(StrictDictionary):
+    authorText:str
+    authorThumbnail:img
+    browseId:str
+
+
+class Comment(StrictDictionary):
+    commentId:str
+    text:str
+    author:authorSimple
+    publishedOn:publishTime
+    replyCount:int
+    isLiked:bool
+    replyContinuation:str
+    authorIsChannelOwner:bool
+    voteStatus:str
+    voteCountApprox:int
+
+class continuationToken(str):
+    def __new__(cls,data=str()):
+        return super().__new__(cls,data)
+
+
+class Comments(StrictDictionary):
+    count:int
+    List:List[Union(Comment,continuationToken)]
